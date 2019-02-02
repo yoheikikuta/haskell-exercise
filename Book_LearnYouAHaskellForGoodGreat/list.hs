1759412eed3b23465db4863b0e9f3e8e8cc28853
yoheikikuta <diracdiego@gmail.com>
2019-02-02 23:13:58 +0900

    Change to use symbol for function name in list.hs

--- Book_LearnYouAHaskellForGoodGreat/list.hs
+++ Book_LearnYouAHaskellForGoodGreat/list.hs
@@ -1,1 +1,3 @@
-data List a = Empty | Cons { listHead :: a, listTail :: List a} deriving (Show, Read, Eq, Ord)
+infixr 5 :-:
+-- data List a = Empty | Cons { listHead :: a, listTail :: List a} deriving (Show, Read, Eq, Ord)
+data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)

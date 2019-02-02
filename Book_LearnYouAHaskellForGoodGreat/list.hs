a6d6b1c5d55c41d64dae4662755711da191b59be
yoheikikuta <diracdiego@gmail.com>
2019-02-02 23:17:49 +0900

    Define list addition in list.hs

--- Book_LearnYouAHaskellForGoodGreat/list.hs
+++ Book_LearnYouAHaskellForGoodGreat/list.hs
@@ -1,3 +1,8 @@
 infixr 5 :-:
+infixr 5 ^++
 -- data List a = Empty | Cons { listHead :: a, listTail :: List a} deriving (Show, Read, Eq, Ord)
 data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)
+
+(^++) :: List a -> List a -> List a
+Empty ^++ ys = ys
+(x :-: xs) ^++ ys = x :-: (xs ^++ ys)

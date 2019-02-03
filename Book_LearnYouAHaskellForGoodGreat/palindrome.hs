d4133ddcd36e66ff89bb532fcac95419bf13b554
yoheikikuta <diracdiego@gmail.com>
2019-02-04 01:42:52 +0900

    Add palindrome

---
+++ Book_LearnYouAHaskellForGoodGreat/palindrome.hs
@@ -0,0 +1,10 @@
+respondPalindromes :: String -> String
+respondPalindromes =
+    unlines .
+    map (\xs -> if isPal xs then "palindrome" else "not palindrome") .
+    lines
+
+isPal :: String -> Bool
+isPal xs = xs == reverse xs
+
+main = interact respondPalindromes

038dd0ca67a1683f820792e02aaa943c1e72d2b4
yoheikikuta <diracdiego@gmail.com>
2019-02-04 01:38:25 +0900

    Change to use interact

--- Book_LearnYouAHaskellForGoodGreat/showshortline.hs
+++ Book_LearnYouAHaskellForGoodGreat/showshortline.hs
@@ -1,6 +1,7 @@
-main = do
-    contents <- getContents
-    putStr (shortLinesOnly contents)
+--main = do
+--    contents <- getContents
+--    putStr (shortLinesOnly contents)
+main = interact shortLinesOnly
 
 shortLinesOnly :: String -> String
 shortLinesOnly = unlines . filter (\line -> length line < 10) . lines

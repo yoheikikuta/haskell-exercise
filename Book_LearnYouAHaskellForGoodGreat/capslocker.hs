fb1192c728e22618f78a169b463b06fbfb6ae141
yoheikikuta <diracdiego@gmail.com>
2019-02-04 01:28:23 +0900

    Change to use getContents

---
+++ Book_LearnYouAHaskellForGoodGreat/capslocker.hs
@@ -0,0 +1,9 @@
+import Control.Monad
+import Data.Char
+
+--main = forever $ do
+--    l <- getLine
+--    putStrLn $ map toUpper l
+main = do
+    contents <- getContents
+    putStr $ map toUpper contents

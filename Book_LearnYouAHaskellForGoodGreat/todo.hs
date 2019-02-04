a25007217e2ba3bc2387f880724c37903994579c
yoheikikuta <diracdiego@gmail.com>
2019-02-05 01:27:38 +0900

    Complete todo

--- Book_LearnYouAHaskellForGoodGreat/todo.hs
+++ Book_LearnYouAHaskellForGoodGreat/todo.hs
@@ -2,11 +2,12 @@ import System.Environment
 import System.Directory
 import System.IO
 import Data.List
+import Control.Exception
 
 dispatch :: String -> [String] -> IO ()
 dispatch "add" = add
 dispatch "view" = view
---dispatch "remove" = remove
+dispatch "remove" = remove
 
 main = do
     (command:argList) <- getArgs
@@ -21,3 +22,22 @@ view [fileName] = do
     let todoTasks = lines contents
         numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] todoTasks
     putStr $ unlines numberedTasks
+
+remove :: [String] -> IO ()
+remove [fileName, numberString] = do
+    contents <- readFile fileName
+    let todoTasks = lines contents
+        numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] todoTasks
+    putStrLn "These are your TO-DO items:"
+    mapM_ putStrLn numberedTasks
+    let number = read numberString
+        newTodoItems = unlines $ delete (todoTasks !! number) todoTasks
+    bracketOnError (openTempFile "." "temp")
+        (\(tempName, tempHandle) -> do
+            hClose tempHandle
+            removeFile tempName)
+        (\(tempName, tempHandle) -> do
+            hPutStr tempHandle newTodoItems
+            hClose tempHandle
+            removeFile fileName
+            renameFile tempName fileName)

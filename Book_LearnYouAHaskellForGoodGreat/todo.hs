8f0438027b762f5c49a3a4c7442423f3f21917da
yoheikikuta <diracdiego@gmail.com>
2019-02-05 01:11:54 +0900

    [WIP] Add todo

---
+++ Book_LearnYouAHaskellForGoodGreat/todo.hs
@@ -0,0 +1,23 @@
+import System.Environment
+import System.Directory
+import System.IO
+import Data.List
+
+dispatch :: String -> [String] -> IO ()
+dispatch "add" = add
+dispatch "view" = view
+--dispatch "remove" = remove
+
+main = do
+    (command:argList) <- getArgs
+    dispatch command argList
+
+add :: [String] -> IO ()
+add [fileName, todoItem] = appendFile fileName (todoItem ++ "\n")
+
+view :: [String] -> IO ()
+view [fileName] = do
+    contents <- readFile fileName
+    let todoTasks = lines contents
+        numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] todoTasks
+    putStr $ unlines numberedTasks

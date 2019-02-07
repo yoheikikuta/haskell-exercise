1f52bedf070bdb802d67b56e23f292d3e64fcc9b
yoheikikuta <diracdiego@gmail.com>
2019-02-07 15:13:36 +0900

    Change rpn to use Maybe Monad to handle error

--- Book_LearnYouAHaskellForGoodGreat/rpn.hs
+++ Book_LearnYouAHaskellForGoodGreat/rpn.hs
@@ -1,13 +1,21 @@
 import Data.List
+import Control.Monad
 
-solveRPN :: String -> Double
-solveRPN = head . foldl foldingFunction [] . words
+solveRPN :: String -> Maybe Double
+solveRPN st = do
+    [result] <- foldM foldingFunction [] (words st)
+    return result
 
-foldingFunction :: [Double] -> String -> [Double]
-foldingFunction (x:y:ys) "*" = (y * x):ys
-foldingFunction (x:y:ys) "+" = (y + x):ys
-foldingFunction (x:y:ys) "-" = (y - x):ys
-foldingFunction (x:y:ys) "/" = (y / x):ys
-foldingFunction (x:xs) "ln" = log x:xs
-foldingFunction xs "sum" = [sum xs]
-foldingFunction xs numberString = read numberString:xs
+readMaybe :: (Read a) => String -> Maybe a
+readMaybe st = case reads st of 
+                    [(x, "")] -> Just x
+                    _ -> Nothing 
+
+foldingFunction :: [Double] -> String -> Maybe [Double]
+foldingFunction (x:y:ys) "*" = return $ (y * x):ys
+foldingFunction (x:y:ys) "+" = return $ (y + x):ys
+foldingFunction (x:y:ys) "-" = return $ (y - x):ys
+foldingFunction (x:y:ys) "/" = return $ (y / x):ys
+foldingFunction (x:xs) "ln" = return $ log x:xs
+foldingFunction xs "sum" = return $ [sum xs]
+foldingFunction xs numberString = liftM (:xs) (readMaybe numberString)

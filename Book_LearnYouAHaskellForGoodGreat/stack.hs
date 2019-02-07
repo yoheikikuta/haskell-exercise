de225562d4e5bc139baab3e304fed37e9736b2e4
yoheikikuta <diracdiego@gmail.com>
2019-02-07 14:30:36 +0900

    Use state monad

--- Book_LearnYouAHaskellForGoodGreat/stack.hs
+++ Book_LearnYouAHaskellForGoodGreat/stack.hs
@@ -1,13 +1,27 @@
+import Control.Monad.State
+
 type Stack = [Int]
 
-pop :: Stack -> (Int, Stack)
-pop (x:xs) = (x, xs)
+pop :: State Stack Int
+pop = state $ \(x:xs) -> (x, xs)
 
-push :: Int -> Stack -> ((), Stack)
-push a xs = ((), a:xs)
+push :: Int -> State Stack ()
+push a = state $ \xs -> ((), a:xs)
 
-stackManip :: Stack -> (Int, Stack)
-stackManip stack = let
-    ((), newStack1) = push 3 stack
-    (a, newStack2)  = pop newStack1
-    in pop newStack2
+stackManip :: State Stack Int
+stackManip = do
+    push 3
+    pop
+    pop
+
+--pop :: Stack -> (Int, Stack)
+--pop (x:xs) = (x, xs)
+--
+--push :: Int -> Stack -> ((), Stack)
+--push a xs = ((), a:xs)
+--
+--stackManip :: Stack -> (Int, Stack)
+--stackManip stack = let
+--    ((), newStack1) = push 3 stack
+--    (a, newStack2)  = pop newStack1
+--    in pop newStack2

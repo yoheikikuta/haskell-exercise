0ae15bee00fa29cc7e4c6ba0741f00709e4ca8bf
yoheikikuta <diracdiego@gmail.com>
2019-02-07 15:02:56 +0900

    Cut out foldingfunction in rpn

--- Book_LearnYouAHaskellForGoodGreat/rpn.hs
+++ Book_LearnYouAHaskellForGoodGreat/rpn.hs
@@ -1,9 +1,13 @@
+import Data.List
+
 solveRPN :: String -> Double
 solveRPN = head . foldl foldingFunction [] . words
-    where foldingFunction (x:y:ys) "*" = (y * x):ys
-          foldingFunction (x:y:ys) "+" = (y + x):ys
-          foldingFunction (x:y:ys) "-" = (y - x):ys
-          foldingFunction (x:y:ys) "/" = (y / x):ys
-          foldingFunction (x:xs) "ln" = log x:xs
-          foldingFunction xs "sum" = [sum xs]
-          foldingFunction xs numberString = read numberString:xs
+
+foldingFunction :: [Double] -> String -> [Double]
+foldingFunction (x:y:ys) "*" = (y * x):ys
+foldingFunction (x:y:ys) "+" = (y + x):ys
+foldingFunction (x:y:ys) "-" = (y - x):ys
+foldingFunction (x:y:ys) "/" = (y / x):ys
+foldingFunction (x:xs) "ln" = log x:xs
+foldingFunction xs "sum" = [sum xs]
+foldingFunction xs numberString = read numberString:xs
